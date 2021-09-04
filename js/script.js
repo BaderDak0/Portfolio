3b8bdba45813059b6399df9a9ccf6d8525557682
bader <baderdaka28@gmail.com>
2021-09-04 13:14:34 +0300

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+function updateicon() {
+
+    $.getJSON("data/bader-portfolio.json", function (data) {
+        console.log(data.media[1].linkedinlink);
+        console.log(data.aboutme.length)
+        $(".linkedin > a").attr("href", data.media[1].linkedinlink);
+        $(".linkedin i").addClass(data.media[1].linkedinicon);
+        $(".facebook > a").attr("href", data.media[0].facebooklink);
+        $(".facebook i").addClass(data.media[0].facebookicon);
+        $(".github > a").attr("href", data.media[2].githublink);
+        $(".github i").addClass(data.media[2].giticon);
+
+        for (var i = 0; i < data.aboutme.length ; i++) {
+            $(".about-us-list").append("<li class='points'>"+data.aboutme[i].list+"</li>");
+        }
+    });
+
+
+}
+
+
+
+
+
+$(document).ready(function () {
+
+    updateicon();
+
+    
+    $( "#CV" ).click(function() {
+        window.open('contact/BaderDakaCV.pdf', '_blank');
+      });
+
+
+
+
+
+});
